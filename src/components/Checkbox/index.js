3fd8411b51177c1250fa017165b71e2198789d1a
peterianmuller <peterianmuller@gmail.com>
2020-04-19 15:04:23 -0700

    Ql 6 build checkbox (#6)
    * add basic checkbox component
    
    * Add story for basic checkbox
    
    * finish basic checkbox with label component
    
    * Update src/components/Checkbox/index.js
    
    Co-Authored-By: Marcus Svehlak <21050133+svey@users.noreply.github.com>
    
    * Update src/components/Checkbox/index.js
    
    Co-authored-by: Marcus Svehlak <21050133+svey@users.noreply.github.com>

---
+++ src/components/Checkbox/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// display error and correct states 
+
+export const CheckBox = ({ onClick, id, name, value, error, success, label, ...rest }) => {
+  return (
+    <label>
+      {/*@todo error and success states still need to be handled */}
+      <input type="checkbox" id={id} name={name} value={value} onClick={onClick} error={error} success={success} label={label} />
+      {label}
+    </label>
+  );
+}
+
+CheckBox.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.bool
+};
+
+CheckBox.defaultProps = {
+  value: false
+};
+
+export default CheckBox;
---
+++ stories/2-Checkbox.stories.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import { CheckBox } from '../src/components/Checkbox';
+
+export default {
+  title: 'CheckBox',
+};
+
+export const Checkbox = () => <CheckBox onClick={() => { }} id="idtest" name="nametest" label="choice 1" />
+
+
+
+
+
